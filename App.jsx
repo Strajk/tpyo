cd5adb74fb92da7b7a9d1b53830beacc66219b82
strajk <strajk@me.com>
2021-11-01 16:40:00 +0100

    react yo!

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+const styled = require('styled-components') // 🚨 require -> import
+
+class NotApp extends React.Component {
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    return <>
+      <h1>Hello stranger!</h1>
+    </>
+  }
+}
+
+export default NotApp // 🚨TODO: Incorrectly named
